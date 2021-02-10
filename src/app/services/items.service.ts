77700c6aac8865562f783dac21cc23f72894ef5c
Sylvia <sylviasastre@gmail.com>
2021-02-10 19:41:22 +0100

    Fix import model

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ItemDto } from '../models/itemDto.model';
+import { ItemDto } from '../models/ItemDto.model';
 
 @Injectable({
   providedIn: 'root'
