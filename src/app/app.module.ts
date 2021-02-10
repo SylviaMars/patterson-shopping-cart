b90f9d785259fa6a49fd5585a2b6eed3813330d8
Sylvia <sylviasastre@gmail.com>
2021-02-10 21:33:04 +0100

    Generate showcase nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { CheckoutCartComponent } from './components/checkout-cart/checkout-cart.component';
 import { ShowcaseComponent } from './components/showcase/showcase.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule} from '@angular/material/button';
+import { MatBadgeModule} from '@angular/material/badge';
 
 @NgModule({
   declarations: [
     AppComponent,
     CheckoutCartComponent,
-    ShowcaseComponent
+    ShowcaseComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MatButtonModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
