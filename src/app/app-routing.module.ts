e9ce8c44f642ddf0c93edc89746618f2bd3492cb
Sylvia <sylviasastre@gmail.com>
2021-02-10 19:47:56 +0100

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckoutCartComponent } from './components/checkout-cart/checkout-cart.component';
+import { ShowcaseComponent } from './components/showcase/showcase.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Routes
+  { path: 'showcase', component: ShowcaseComponent},
+  { path: 'cart', component: CheckoutCartComponent},
+  { path: '', component: ShowcaseComponent},
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
