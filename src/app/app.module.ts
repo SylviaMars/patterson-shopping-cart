bbfed133109177e5a329972b9ca0cbb6b538a7fe
Sylvia <sylviasastre@gmail.com>
2021-02-10 19:40:05 +0100

    Generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckoutCartComponent } from './components/checkout-cart/checkout-cart.component';
+import { ShowcaseComponent } from './components/showcase/showcase.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutCartComponent,
+    ShowcaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
